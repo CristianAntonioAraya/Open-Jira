941e0e9950bf2132fe72383117a52443c88a8963
Dev Cristian Araya <dev.arayacristian@gmail.com>
2022-05-17 16:18:56 -0400

    [feat] Add context to manage the global state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { UIProvider } from '../context/ui'
+import { EntriesProvider } from '../context/entries'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <EntriesProvider>
+      <UIProvider>
+        <Component {...pageProps} />
+      </UIProvider>
+    </EntriesProvider>
+  )
+  
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import type { NextPage } from 'next'
-import Layout from '../layouts/Layout'
+import Layout from '../components/layouts/Layout';
+import { EntryList } from '../components/ui';
+import styles from '../styles/pages/home.module.scss';
 
 const Home: NextPage = () => {
   return (
-    <Layout>
-      <p> Hola Mundo </p>
+    <Layout title='Home'>
+      <div className={ styles.container }>
+          <EntryList title='Pending' status='pending'/>
+          <EntryList title='In-Progress' status='in-progress'/>
+          <EntryList title='Finished' status='finished'/>
+      </div>
     </Layout>
   )
 }
